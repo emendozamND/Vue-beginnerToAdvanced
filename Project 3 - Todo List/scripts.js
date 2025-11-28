f2655ca1301e54232a32f29e4bea0aa8fdba7d19
Elias Mendoza <eliasmm@gmail.com>
2025-11-28 11:31:44 -0600

    adding To-Dos

--- Project 3 - Todo List/scripts.js
+++ Project 3 - Todo List/scripts.js
@@ -1,25 +1,44 @@
-
-//3 create  an array to save  the items
+// 3.- create an array to save the items
 var todos = [
-{
+  {
     text: 'Learn HTML, CSS and Javascript',
     done: true
-},
-{
+  },
+  {
     text: 'Learn the basics of Go',
     done: false
-},
+  }
+];
 
- ]
-
-
-//1.- initializing the component
+// 1.- initializing the component
 const todosApp = {
-    data(){
-        return {
-            todos: window.todos
-        }
+  data() {
+    return {
+      // puedes usar window.todos o solo todos
+      todos: window.todos, 
+      newTodo: {
+        text: '',     // texto vacío al inicio
+        done: false
+      }
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.text) {
+        // agregamos un nuevo objeto a la lista
+        this.todos.push({
+          text: this.newTodo.text,
+          done: false
+        });
+
+        // limpiamos el input
+        this.newTodo.text = '';
+      } else {
+        alert("Todo text is required");
+      }
     }
+  }
 };
-//2.- call the instance and  call the mount method to load component in html file
+
+// 2.- call the instance and mount the component in the html file
 Vue.createApp(todosApp).mount('#app');
