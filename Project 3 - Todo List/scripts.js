26a738bd2644b4691899b7f8b91652bbdf0f86f5
Elias Mendoza <eliasmm@gmail.com>
2025-11-27 21:35:04 -0600

    show data dinamically

--- Project 3 - Todo List/scripts.js
+++ Project 3 - Todo List/scripts.js
@@ -1,7 +1,24 @@
+
+//3 create  an array to save  the items
+var todos = [
+{
+    text: 'Learn HTML, CSS and Javascript',
+    done: true
+},
+{
+    text: 'Learn the basics of Go',
+    done: false
+},
+
+ ]
+
+
 //1.- initializing the component
 const todosApp = {
     data(){
-        return {}
+        return {
+            todos: window.todos
+        }
     }
 };
 //2.- call the instance and  call the mount method to load component in html file
