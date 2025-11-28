4e01e8396fda1286e82ceddb1eea12dcd9dc0511
Elias Mendoza <eliasmm@gmail.com>
2025-11-27 20:14:18 -0600

    creating the second proyect using two-Way Data Binding

---
+++ Project 2 - Working With Forms/scripts.js
@@ -0,0 +1,30 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Acoustic Guitar'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Electric Guitar'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members,
+            newMember: {}
+        }
+    },
+    methods:{
+        addMember:function(){
+            this.members.push(this.newMember);
+        }
+    }
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
